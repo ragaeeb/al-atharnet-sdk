23d28e0ac174c475296f79d817bd7a780958777c
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2024-10-03 11:20:55 +0400

    Use wretch for cross-platform compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import wretch from 'wretch';
+import QueryStringAddon from 'wretch/addons/queryString';
+
 import { Page } from './types';
-import { buildUrl, httpsGet } from './utils/network';
 import { parsePage } from './utils/parser';
 
+const w = wretch('https://alathar.net/home/esound').addon(QueryStringAddon);
+
 export const getPage = async (id: number): Promise<Page> => {
-    const response = (await httpsGet(buildUrl({ coid: id, op: 'codevi' }))).toString().trim();
+    const response = await w.url('/index.php').query({ coid: id, op: 'codevi' }).get().text();
 
     if (!response) {
         throw new Error(`Page ${id} not found`);
